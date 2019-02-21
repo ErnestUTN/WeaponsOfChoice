1f5dbe35a21f10b15bd66055defd4de4fc8d5f99
ErnestUTN <ernest.sastre@gmail.com>
2019-02-21 01:28:21 -0300

    Fixed critical exception thrown when saving game
    Scribe_Collections.Look(ref this.SearchPriorityThingDefs, "WPriorityWeapons") should automatically define the lookmode to use (LookMode.Def in this case)

--- Source/WeaponPreset.cs
+++ Source/WeaponPreset.cs
@@ -28,7 +28,7 @@ namespace WeaponsOfChoice
             Scribe_Values.Look<int>(ref this.uniqueId, "WuniqueId", 0, false);
             Scribe_Values.Look<string>(ref this.label, "Wlabel", null, false);
             Scribe_Deep.Look<ThingFilter>(ref this.filter, "Wfilter", new object[0]);
-            Scribe_Collections.Look(ref this.SearchPriorityDefnames, "WPriorityWeapons", LookMode.Deep);
+            Scribe_Collections.Look(ref this.SearchPriorityThingDefs, "WPriorityWeapons");
         }
 
 
